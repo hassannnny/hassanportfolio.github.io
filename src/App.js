5d45ff25f5d46ccdc68398edb74601f2ef7b93f5
hassannnny <lhjaved@neiu.edu>
2024-04-25 14:04:13 -0500

    changing text color to blue

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hassan Javed Portfolio</h1>
+      <h1 style={{color: 'blue'}}>Hassan Javed Portfolio</h1>
     </div>
   );
 }
